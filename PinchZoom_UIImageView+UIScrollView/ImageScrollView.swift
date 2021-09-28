e07c18bab563be13103abba06ba5d39711dc93d3
KimWooSun <kipsong133@gmail.com>
2021-09-28 12:57:42 +0900

    이미지 스케일 크기 제한 및 이미지 정중앙 위치

--- PinchZoom_UIImageView+UIScrollView/ImageScrollView.swift
+++ PinchZoom_UIImageView+UIScrollView/ImageScrollView.swift
@@ -37,6 +37,57 @@ class ImageScrollView: UIScrollView {
     
     func configurateFor(imageSize: CGSize) {
         self.contentSize = imageSize
+        setCurrentMaxAndMinZoomScale()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        self.centerImage()
+    }
+    
+    func setCurrentMaxAndMinZoomScale() {
+        let boundsSize = self.bounds.size
+        let imageSize = imageZoomView.bounds.size
+        
+        let xScale = boundsSize.width / imageSize.width
+        let yScale = boundsSize.height / imageSize.height
+        let minScale = min(xScale, yScale)
+        
+        var maxScale: CGFloat = 1.0
+        if minScale < 0.1 {
+            maxScale = 0.3
+        }
+        
+        if minScale >= 0.1 && minScale < 0.5 {
+            maxScale = 0.7
+        }
+        
+        if minScale >= 0.5 {
+            maxScale = max(1.0, minScale)
+        }
+        
+        self.minimumZoomScale = minScale
+        self.maximumZoomScale = maxScale
+    }
+    
+    func centerImage() {
+        let boundsSize = self.bounds.size
+        var frameToCenter = imageZoomView.frame ?? CGRect.zero
+        
+        if frameToCenter.size.width < boundsSize.width {
+            frameToCenter.origin.x = (boundsSize.width - frameToCenter.size.width) / 2
+        } else {
+            frameToCenter.origin.x = 0
+        }
+        
+        if frameToCenter.size.height < boundsSize.height {
+            frameToCenter.origin.y = (boundsSize.height - frameToCenter.size.height) / 2
+        } else {
+            frameToCenter.origin.y = 0
+        }
+        
+        imageZoomView.frame = frameToCenter
     }
 }
 
