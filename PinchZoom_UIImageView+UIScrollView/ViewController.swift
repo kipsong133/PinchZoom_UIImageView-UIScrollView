a13e89175a05f3a8e2a86b4335113e4e1469b659
KimWooSun <kipsong133@gmail.com>
2021-09-28 12:44:52 +0900

    scrollView에 UIImageView 추가

--- PinchZoom_UIImageView+UIScrollView/ViewController.swift
+++ PinchZoom_UIImageView+UIScrollView/ViewController.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var imageScrollView: ImageScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageScrollView = ImageScrollView(frame: view.bounds)
+        view.addSubview(imageScrollView)
+        setUpImageScrollView()
+        
+        let imagePath = Bundle.main.path(forResource: "crab", ofType: "jpg")!
+        let image = UIImage(contentsOfFile: imagePath)!
+        self.imageScrollView.set(image: image)
+    }
+    
+    func setUpImageScrollView() {
+        imageScrollView.translatesAutoresizingMaskIntoConstraints = false
+        imageScrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        imageScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        imageScrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        imageScrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
     }
 
 
